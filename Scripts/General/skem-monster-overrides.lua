4139d00e477bbda2c53d6b39766f9f1cd580782e
Malekitsu <106842972+Malekitsu@users.noreply.github.com>
2023-04-28 18:18:26 +0200

    Fixed monster if item and stats rework is on

--- Scripts/General/skem-monster-overrides.lua
+++ Scripts/General/skem-monster-overrides.lua
@@ -895,8 +895,15 @@ function events.LoadMap()
 				Map.Monsters[i].Ally = 1
 				Map.Monsters[i].FullHitPoints = Map.Monsters[i].FullHitPoints*2
 				Map.Monsters[i].HitPoints = Map.Monsters[i].HitPoints*2
+				if SETTINGS["ItemRework"]==true and SETTINGS["StatsRework"]==true then	
+					Map.Monsters[i].FullHitPoints = Map.Monsters[i].FullHitPoints * (1+Map.Monsters[i].Level/200)
+					Map.Monsters[i].HitPoints = Map.Monsters[i].HitPoints * (1+Map.Monsters[i].Level/200)
+				end
 			-- bonus damage
 				DamageMultiplier=Map.Monsters[i].Level/20+1.75
+				if SETTINGS["ItemRework"]==true and SETTINGS["StatsRework"]==true then
+					DamageMultiplier=DamageMultiplier*(1+Map.Monsters[i].Level/100)
+				end
 				--attack 1
 				a=Map.Monsters[i].Attack1.DamageAdd * DamageMultiplier
 				Map.Monsters[i].Attack1.DamageAdd = Map.Monsters[i].Attack1.DamageAdd * DamageMultiplier
